4a101ba1f99f7d3e516dc4e83684d8f74c9eca11
farzana <farzanabhuiyan64@gmail.com>
2024-08-11 22:31:12 +0600

    Category Create Edit

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::middleware('auth')->prefix('/dashboard')->controller(UserDashboardControl
 
 Route::get('/category',[CategoryController::class,'index'])->name('category.index');
 Route::get('/category-create',[CategoryController::class,'create'])->name('category.create');
- Route::get('/category-store',[CategoryController::class,'store'])->name('category.store');
+Route::get('/category-store',[CategoryController::class,'store'])->name('category.store');
 Route::get('/category-edit/{id}',[CategoryController::class,'edit'])->name('category.edit');
 // Route::put('/category-update/{id}',[CategoryController::class,'update'])->name('category.update');
 // Route::delete('/category-delete/{id}',[CategoryController::class,'destroy'])->name('category.delete');
